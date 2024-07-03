c2ae5beaafc45dbfef5348c8e642386da25ba3be
FamzzzONTOP <168281239+FamzzzONTOP@users.noreply.github.com>
2024-07-04 02:54:00 +0700

    Update BloxFruit.lua

--- BloxFruit.lua
+++ BloxFruit.lua
@@ -1,3 +1,5 @@
+local SlayerzUI_MobileToggle = loadstring(game:HttpGet("https://raw.githubusercontent.com/x2-Neptune/SlayerzUI/main/MBToggle.script"))()
+SlayerzUI_MobileToggle:Create("rbxassetid://15110884615")
 repeat wait(1) until game:IsLoaded()
 local AlchemyHub_Version = "2.4 [ Free ]"
 local AlchemyHub_UserCalling = game.Players.LocalPlayer.Name
