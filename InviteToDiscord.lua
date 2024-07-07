1b542b0aab1588d86412105e2f3d58c912c63c5e
Medusa <155561619+MedusaScript@users.noreply.github.com>
2024-07-08 02:59:02 +0700

    Update InviteToDiscord.lua

--- InviteToDiscord.lua
+++ InviteToDiscord.lua
@@ -1,6 +1,6 @@
 -- Skid Mati Amin
 local SlayerzUI_Notify = loadstring(game:HttpGet("https://raw.githubusercontent.com/x2-Neptune/SlayerzUI/main/Notification.script"))()
-local NameHub = "BRUTALITY V1"
+local NameHub = "BRUTALITY"
 local Description = "THIS IS BRUTALITY!,By Medusa Script"
 local Logo = "rbxassetid://16058297648"
 local Color = Color3.fromRGB(0, 255, 145)
