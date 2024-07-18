ec73007638fccf6cd44b271973d7b32e1da2151f
Medusa <155561619+MedusaScript@users.noreply.github.com>
2024-07-18 20:22:12 +0700

    Update swordball1.lua

--- swordball1.lua
+++ swordball1.lua
@@ -1,8 +1,37 @@
-spawn(function()
-    while true do
-        wait(0.01)
-        if getgenv().ASC then
-            game:GetService("ReplicatedStorage").Remote.RemoteFunction:InvokeServer("PromptPurchaseCrate", workspace.Spawn.Crates.NormalSwordCrate)
+function play_kill_effect(Part)
+    task.defer(function()
+        local bell = game:GetObjects("rbxassetid://17519762269")[1]
+
+        bell.Name = 'Yeat_BELL'
+        bell.Parent = workspace
+
+        bell.Position = Part.Position - Vector3.new(0, 20, 0)
+        bell:WaitForChild('Sound'):Play()
+
+        game:GetService("TweenService"):Create(bell, TweenInfo.new(0.85, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
+            Position = Part.Position + Vector3.new(0, 10, 0)
+        }):Play()
+
+        task.delay(5, function()
+            game:GetService("TweenService"):Create(bell, TweenInfo.new(1.45, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
+                Position = Part.Position + Vector3.new(0, 100, 0)
+            }):Play()
+        end)
+
+        task.delay(6, function()
+            bell:Destroy()
+        end)
+    end)
+end
+
+task.defer(function()
+    workspace.Alive.ChildRemoved:Connect(function(child)
+        if not workspace.Dead:FindFirstChild(child.Name) then
+            return
         end
-    end
+
+        if getgenv().kill_effect_Enabled then
+            play_kill_effect(child.HumanoidRootPart)
+        end
+    end)
 end)
