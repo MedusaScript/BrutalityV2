9085af97ed91ccadc0ebecef4f78c79eb16852e3
Medusa <155561619+MedusaScript@users.noreply.github.com>
2024-07-20 04:46:15 +0700

    Update Sound.lua

--- Sound.lua
+++ Sound.lua
@@ -1,9 +1,17 @@
-getgenv().hit_sound_Enabled = true
+getgenv().aura_Enabled = false
+getgenv().hit_sound_Enabled = false
 getgenv().hit_effect_Enabled = false
-getgenv().kill_effect_Enabled = true
+getgenv().night_mode_Enabled = false
+getgenv().trail_Enabled = false
+getgenv().self_effect_Enabled = false
+getgenv().kill_effect_Enabled = false
+getgenv().shaders_effect_Enabled = false
+getgenv().ai_Enabled = false
+getgenv().spectate_Enabled = false
+
 local Services = {
-    game:GetService('AdService'),
-    game:GetService('SocialService')
+	game:GetService('AdService'),
+	game:GetService('SocialService')
 }
 
 function initializate(dataFolder_name: string)
